d4af84ec5bd95d3d3fc3697525ee6a7f00e8b8ba
konikayadav <134596497+konikayadav@users.noreply.github.com>
2023-12-18 18:58:22 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload()
+{
+
+}
+function setup()
+{
+    Canvas = createCanvas(500,400);
+    Canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw()
+{
+
+}
